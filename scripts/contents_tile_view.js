3d81141513d607c73d6a61355f0b1316684aff86
Biacco42 <biacco42@gmail.com>
2022-02-20 15:53:12 +0900

    Remove observation when the card is shown

--- scripts/contents_tile_view.js
+++ scripts/contents_tile_view.js
@@ -136,6 +136,8 @@ export default class ContentsTileView {
                             card.show()
                         }, delay)
                     }
+
+                    this.cardIntersectionObserver.unobserve(entry.target)
                 }
             })
         }
